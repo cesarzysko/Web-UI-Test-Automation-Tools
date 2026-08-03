bf41570fac2f3554d2f85838d771e33a59656525
_cesarz <mik.noga@proton.me>
2026-08-03 22:03:09 +0200

    Log4NetUtils -> Log4NetExtensions

---
+++ Core/Extensions/Log4NetExtensions.cs
@@ -0,0 +1,20 @@
+using log4net.Core;
+
+namespace Core;
+
+public static class Log4NetExtensions
+{
+    extension(LogLevel logLevel)
+    {
+        public Level ToLog4NetLevel()
+        {
+            return logLevel switch
+            {
+                LogLevel.Info => Level.Info,
+                LogLevel.Warn => Level.Warn,
+                LogLevel.Error => Level.Error,
+                _ => throw new ArgumentOutOfRangeException(nameof(logLevel), logLevel, null)
+            };
+        }
+    }
+}
